11426b7b07a9868006f16ee2531f3d1da573e88f
xiaohua0877 <xiaohua0877@sina.com>
2017-12-17 18:00:03 +0800

    test file ok  core/bus.c test again

--- drivers/mmc/core/bus.c
+++ drivers/mmc/core/bus.c
@@ -7,6 +7,8 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
+ test file 
+ test file  mmc driver model 
  *
  *  MMC card bus driver model
  */
